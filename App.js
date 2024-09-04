99e9cdbe273d1d9844a4b9198e827f35b8f719d3
Pradeep Detroja <detrojapradeep111@gmail.com>
2024-09-04 08:52:48 +0100

    Hello World using react cdn

---
+++ App.js
@@ -0,0 +1,23 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child1" },
+    [
+      React.createElement("h1", {}, "I am the H1 from child1"),
+      React.createElement("h2", {}, "I am the H2 from child1"),
+    ]),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    [
+      React.createElement("h1", {}, "I am the H1 from child2"),
+      React.createElement("h2", {}, "I am the H2 from child2"),
+    ])]
+);
+
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
